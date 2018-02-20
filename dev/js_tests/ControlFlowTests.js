635b396140f872e8a7da77b492be9e5ebacc48b8
Magnus Strandh <magnus.strandh@king.com>
2018-02-20 09:13:32 +0100

    Updated tests

--- dev/js_tests/ControlFlowTests.js
+++ dev/js_tests/ControlFlowTests.js
@@ -1,8 +1,14 @@
 describe("ControlFlowTests", function() {
 
-	it("can compare names", function() {
+	it("can compare names that are not equal", function() {
 		var returnValue = areNamesEqual("Leif", "Berit");
 
 		expect(returnValue).toBe(false);
 	});
+
+	// it("can compare names that are equal", function() {
+	// 	var returnValue = areNamesEqual("Leif", "Leif");
+
+	// 	expect(returnValue).toBe(true);
+	// });
 });
