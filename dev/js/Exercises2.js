3d03d83e3c94770a3f06b03eaeb3ffc8d62e772d
Magnus Strandh <magnus.strandh@king.com>
2018-02-28 11:17:22 +0100

    New exercises - Control Flow

---
+++ dev/js/Exercises2.js
@@ -0,0 +1,83 @@
+/****************************************************************************************
+* 									Control Flow
+****************************************************************************************/
+
+// -------------------------------------------------------------------------------------
+// Example #1: Basic control flow
+// Question #1: What do you expect it to output?
+// Question #2: What does it actually output?
+// Question #3: What else can it output?
+// Question #4: How do you make it output the other texts?
+// -------------------------------------------------------------------------------------
+
+// function spawnDaemon(isLarge) {
+// 	if(isLarge === true) {
+// 		console.log("You spawn a large daemon");
+// 	} else {
+// 		console.log("You spawn a small daemon");
+// 	}
+// }
+// spawnDaemon(false);
+
+// -------------------------------------------------------------------------------------
+
+
+// -------------------------------------------------------------------------------------
+// Example #2: Basic control flow
+// Question #1: What do you expect it to output?
+// Question #2: What does it actually output?
+// Question #3: What else can it output?
+// Question #4: How do you make it output the other texts?
+// -------------------------------------------------------------------------------------
+
+// function castSpell(spellType) {
+// 	if(spellType === "Fireball") {
+// 		console.log("You cast a fireball");
+
+// 	} else if(spellType === "Lightning") {
+// 		console.log("You shoot lightning from your fingers");
+
+// 	} else {
+// 		console.log("You stare at a tree like a goat");
+// 	}
+// }
+
+// castSpell("Lightning");
+
+// -------------------------------------------------------------------------------------
+
+
+// -------------------------------------------------------------------------------------
+// Example #3: Basic control flow
+// Question #1: What do you expect it to output?
+// Question #2: What does it actually output?
+// Question #3: What else can it output?
+// Question #4: How do you make it output the other texts?
+// Question #5: When is the parameter 'spellType' used? Under what conditions?
+// -------------------------------------------------------------------------------------
+
+// function castSpellAdvanced(weakSpell, spellType) {
+// 	if(weakSpell === true) {
+// 		console.log("You cast a bucket of water at your foe");
+
+// 	} else {
+// 		if(spellType === "Fungus") {
+// 			console.log("You summon a blizzard of fungi on your foe");
+
+// 		} else if(spellType === "Lava") {
+// 			console.log("You rain lava on your foe");
+
+// 		} else {
+// 			console.log("You attempt to cast a powerful spell but can't decide which - your brain melts");
+// 		}
+// 	}
+	
+// }
+
+// castSpellAdvanced(true, "Lava");
+
+// -------------------------------------------------------------------------------------
+
+
+
+
