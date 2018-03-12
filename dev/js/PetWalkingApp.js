2e497cee0a762fcd51a992cca959538ec955e1b8
Freja Hillström <frejahillstrom@gmail.com>
2018-03-12 10:58:45 +0100

    generaterandomdog

--- dev/js/PetWalkingApp.js
+++ dev/js/PetWalkingApp.js
@@ -1,1 +1,19 @@
-console.log("PetWalking");
+var dogBreeds = ["German Shepherd", "Dachshund", "Icelandic Sheepdog", "Beagle", "Scottish Terrier", "French Bulldog", "Dobermann", "Collie", "Dalmatiner", "Pointer", "Welsh springer spaniel", "Griffon Bruxellois", "Pug", "Whippet"];
+var printOutDogBreed = document.getElementsByTagName("h1");
+
+var dogIndex = getRandomDog();
+var selectedDog = dogBreeds[dogIndex];
+var numDogs = dogBreeds.length;
+
+console.log("Selected dog: " + selectedDog);
+console.log("Num dogs: " + numDogs);
+
+function getRandomDog() {
+	return 7;
+}
+
+
+var generateRandomDog = dogBreeds[Math.floor(Math.random() * dogBreeds.length)];
+
+	// * 10) + 1);
+console.log(generateRandomDog);
