0bc9aee72edb6f390ddeb2abd75a4809eb518b27
Magnus Strandh <magnus.strandh@king.com>
2018-03-13 11:44:26 +0100

    New exercises

---
+++ dev/js/Exercises4.js
@@ -0,0 +1,10 @@
+/*********************************************************************************************
+* Write a function that accepts a string as parameter and returns an array
+* Hint: str.split(" ")
+**********************************************************************************************
+* Output:
+* ["Leif", "is", "very", "angry", "today"]
+**********************************************************************************************/
+
+var output = stringToArray("Leif is very angry today");
+console.log(output);
